31afc8ae9eb60f987417881d46a47980a3cda3e9
endahrakhma <endahen12@gmail.com>
2024-08-02 21:39:25 +0700

    revisi

--- breastcancer_app.py
+++ breastcancer_app.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import joblib
 import base64
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from keras.models import load_model
 
@@ -18,7 +21,7 @@ st.markdown('---'*10)
 model_final = joblib.load('breastcancer_pipeline.pkl')
 model_final.named_steps['modeling'].model = load_model('model_bc_keras.h5')
 
-pilihan = st.selectbox('Apa yang ingin Anda lakukan?',['Prediksi dari file excel','Input Manual'])
+pilihan = st.selectbox('Apa yang ingin Anda lakukan?',['Prediksi dari file excel','Input Manual','Data Visualisasi'])
 
 if pilihan == 'Prediksi dari file excel':
     def set_bg_1(main_bg):
@@ -81,7 +84,7 @@ if pilihan == 'Prediksi dari file excel':
     else:
         st.error('File yang diupload kosong, silakan pilih file yang valid')
         #st.markdown('File yang diupload kosong, silakan pilih file yang valid')
-else:
+elif pilihan == 'Input Manual':
     def set_bg_2(main_bg):
 
         main_bg_ext = "png"
@@ -239,4 +242,97 @@ else:
             st.write(mystyle,'Malignant',unsafe_allow_html=True)
         else:
             st.write(mystyle,'Benign',unsafe_allow_html=True)
+else:
+    #st.set_page_config(page_title='Data Visualizer',
+    #               layout='centered',
+    #               page_icon='📊')
+
+    # Title
+    #st.write("<h4 style='text-align: center;'>📊  Visualisasi Data</h4>", unsafe_allow_html=True)
+    #st.markdown('📊  Data Visualizer')
     
+    def set_bg_3(main_bg):
+
+        main_bg_ext = "png"
+            
+        st.markdown(
+             f"""
+             <style>
+             .stApp {{
+                 background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+                 background-position: center;
+                 background-color: white;
+                 background-size: 90px 70px;
+                 background-repeat: repeat;
+
+             }}
+             </style>
+             """,
+             unsafe_allow_html=True
+         )
+    set_bg_3('Floral_Breast_Cancer_Ribbon-fixed.png')
+    
+    working_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Specify the folder where your CSV files are located
+    folder_path = f"{working_dir}"  # Update this to your folder path
+    
+    # List all files in the folder
+    files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+    
+    # Dropdown to select a file
+    #selected_file = st.selectbox('Select a file', files, index=None)
+    st.write('Dataset : ',files[0])
+    
+    if files[0]:
+        # Construct the full path to the file
+        file_path = os.path.join(folder_path, files[0])
+    
+        # Read the selected CSV file
+        df = pd.read_csv(file_path)
+    
+        col1, col2 = st.columns(2)
+    
+        columns = df.columns.tolist()
+    
+        with col1:
+            st.write("")
+            st.write(df.head())
+    
+        with col2:
+            # Allow the user to select columns for plotting
+            x_axis = st.selectbox('Select the X-axis', options=columns+["None"])
+            y_axis = st.selectbox('Select the Y-axis', options=columns+["None"])
+    
+            plot_list = ['Scatter Plot', 'Distribution Plot', 'Count Plot']
+            # Allow the user to select the type of plot
+            plot_type = st.selectbox('Select the type of plot', options=plot_list)
+    
+        # Generate the plot based on user selection
+        if st.button('Generate Plot'):
+    
+            fig, ax = plt.subplots(figsize=(6, 4))
+    
+            if plot_type == 'Scatter Plot':
+                sns.scatterplot(x=df[x_axis], y=df[y_axis], ax=ax)
+            elif plot_type == 'Distribution Plot':
+                sns.histplot(df[x_axis], kde=True, ax=ax)
+                y_axis='Density'
+            elif plot_type == 'Count Plot':
+                sns.countplot(x=df[x_axis], ax=ax)
+                y_axis = 'Count'
+    
+            # Adjust label sizes
+            ax.tick_params(axis='x', labelsize=10)  # Adjust x-axis label size
+            ax.tick_params(axis='y', labelsize=10)  # Adjust y-axis label size
+    
+            # Adjust title and axis labels with a smaller font size
+            plt.title(f'{plot_type} of {y_axis} vs {x_axis}', fontsize=12)
+            plt.xlabel(x_axis, fontsize=10)
+            plt.ylabel(y_axis, fontsize=10)
+    
+            # Show the results
+            st.pyplot(fig)
+
+
+        
